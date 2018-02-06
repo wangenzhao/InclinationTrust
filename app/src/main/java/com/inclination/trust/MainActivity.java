514102f0d58e183b369ea19fd436cdce68773494
wangenzhao <wangenzhao2016@gmail.com>
2018-02-06 23:14:57 +0800

    新增设置页面

--- app/src/main/java/com/inclination/trust/MainActivity.java
+++ app/src/main/java/com/inclination/trust/MainActivity.java
@@ -1,6 +1,10 @@
 package com.inclination.trust;
 
+import android.content.Intent;
+import android.view.View;
+
 import com.inclination.trust.base.BaseActivity;
+import com.inclination.trust.ui.SettingActivity;
 
 public class MainActivity extends BaseActivity {
 
@@ -12,5 +16,11 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initView() {
 
+    }
+
+    public void openSetting(View v) {
+
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/inclination/trust/ui/SettingActivity.java
@@ -0,0 +1,21 @@
+package com.inclination.trust.ui;
+
+import com.inclination.trust.R;
+import com.inclination.trust.base.BaseActivity;
+
+/**
+ * author:王恩钊
+ * date:2018/2/6 22:47
+ */
+
+public class SettingActivity extends BaseActivity {
+    @Override
+    public int getLayout() {
+        return R.layout.activity_setting;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
